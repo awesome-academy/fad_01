13096c9acbdc1d30e22c720e489f7673f6505f28
bichht-0889 <hoang.thi.bich@sun-asterisk.com>
2019-05-20 16:34:40 +0700

    devise

--- db/migrate/20190517093636_create_users.rb
+++ db/migrate/20190517093636_create_users.rb
@@ -6,7 +6,7 @@ class CreateUsers < ActiveRecord::Migration[5.2]
       t.string :address
       t.string :phone
       t.integer :role, default: 0
-      t.string :password_digest
+      # t.string :password_digest
       t.string :remember_digest
       t.string :activation_digest
       t.boolean :activated, default: false
